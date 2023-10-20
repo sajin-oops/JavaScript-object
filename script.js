9482a4c78589d4d3c56ebc1dc75f021104090167
A.K.SAJIN <108419506+sajin-oops@users.noreply.github.com>
2023-10-20 18:28:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+
+// Another way to create object
+
+let item2 = new Object();
+item2.name = "iphone"
+item2.price = 90000
+item2.quantity = 1
+item2.color = "black"
+console.log(item2)
+
+// output - {name: 'iphone', price: 90000, quantity: 1, color: 'black'}
